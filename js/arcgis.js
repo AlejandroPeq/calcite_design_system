518f5edf7ec2eedd26a60b56670919e0a51c73ef
Alejandro <alejandropequenopulleiro@gmail.com>
2022-10-05 16:01:41 +0200

    Adding new widgets

--- js/arcgis.js
+++ js/arcgis.js
@@ -6,8 +6,10 @@ require([
     "esri/widgets/LayerList",
     "esri/widgets/Legend",
     "esri/widgets/Slice",
-    "esri/widgets/Weather"
-], function (WebScene, SceneView, Bookmarks, BasemapGallery, LayerList, Legend, Slice, Weather) {
+    "esri/widgets/Weather",
+    "esri/widgets/DirectLineMeasurement3D",
+    "esri/widgets/AreaMeasurement3D",
+], function (WebScene, SceneView, Bookmarks, BasemapGallery, LayerList, Legend, Slice, Weather,DirectLineMeasurement3D,AreaMeasurement3D) {
 
     const websceneId = 'cac6143eca14471ebc1b90ea29b827c6'
 
@@ -30,9 +32,14 @@ require([
 
     const weather = new Weather({
         view: view,
+        container:'weather-container'
     });
 
-    view.ui.add(weather, "top-right");
+    let measurementWidget = new AreaMeasurement3D({
+        view: view,
+        container:'area-measurement-container'
+      });
+    
 
     webscene.when(() => {
 
@@ -41,5 +48,20 @@ require([
         document.querySelector("calcite-loader").active = false;
 
     });
+
+    let left_panel = document.getElementById('left-panel');
+
+    document.getElementById('collapse-button').addEventListener("click", function(){
+
+        if (left_panel.getAttribute("collapsed") == "false")
+        
+            left_panel.setAttribute("collapsed", "true"); 
+        else
+            left_panel.setAttribute("collapsed", "false");
+        
+
+    });
+
+    
 
 });
